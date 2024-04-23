29e61586aea54af5970f526ebce125514a1af64d
eli-bans <elikembansah@gmail.com>
2024-04-23 06:46:52 +0000

    arithmetic and type casting

--- main.rs
+++ main.rs
@@ -1,11 +1,13 @@
-use std:: io;
+use std::io;
 fn main() {
-   // let arr :[u32 ; 4 ]   = [1,2,3,4];
-   //  println!("{}",arr[1]);
-    let mut input = String :: new(); //declare an empty string
-    println!("Almost forgot this thing ");
-    println!("Enter an input? ");
 
-    io::stdin().read_line(&mut input).expect("Failed read");
-    println!("Here's your string {}",input);
+    //arithmetic
+    // you can't add different types together
+    let mut input = String::new();
+    println!("Enter a number ");
+    io::stdin().read_line(&mut input).expect("Failed reading input");
+
+    let int_input: u64 = input.trim().parse().unwrap();
+    println!("{}",int_input + 2_u64);
+
 }
