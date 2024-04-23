0fdfd660296e2631f6b1b75f49c55d22144ac304
eli-bans <elikembansah@gmail.com>
2024-04-23 06:19:00 +0000

    tooke input from console

--- main.rs
+++ main.rs
@@ -1,4 +1,11 @@
+use std:: io;
 fn main() {
-   let arr :[u32 ; 4 ]   = [1,2,3,4];
-    println!("{}",arr[1]);
+   // let arr :[u32 ; 4 ]   = [1,2,3,4];
+   //  println!("{}",arr[1]);
+    let mut input = String :: new(); //declare an empty string
+    println!("Almost forgot this thing ");
+    println!("Enter an input? ");
+
+    io::stdin().read_line(&mut input).expect("Failed read");
+    println!("Here's your string {}",input);
 }
